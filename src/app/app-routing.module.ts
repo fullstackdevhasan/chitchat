09aeeb1472a4b005b59b6c0f7868bbf0cf46c167
soniya <soniya@centire.in>
2020-08-07 14:53:13 +0530

    Add new component, add styles, update package.json file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { UsersigninComponent } from './usersignin/usersignin.component';
+import { SliderComponent } from './slider/slider.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:"userlogin", component: UserloginComponent},
+  {path:"usersignin", component: UsersigninComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
+
+ 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SliderComponent } from './slider/slider.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { UsersigninComponent } from './usersignin/usersignin.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    UserloginComponent,
+    UsersigninComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
